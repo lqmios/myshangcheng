9ee98fff61830dc10406f6b150c5b9c5e2d39301
lqm <lqm@lqmdemacbook-pro.local>
2016-08-10 13:18:44 +0800

    Initial Commit

---
+++ mmmmm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mmmmm
+//
+//  Created by lqm on 16/8/10.
+//  Copyright © 2016年 LQM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ mmmmm/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mmmmm
+//
+//  Created by lqm on 16/8/10.
+//  Copyright © 2016年 LQM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
